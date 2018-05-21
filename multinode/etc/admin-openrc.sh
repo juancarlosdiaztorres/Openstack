d4b1da42aea2cfbb28b71e5dd2a7cb81d3bf83e9
juancarlosdiaztorres <jcdiaztorres96@gmail.com>
2018-05-21 08:00:30 +0200

    Add files via upload

---
+++ multinode/etc/admin-openrc.sh
@@ -0,0 +1,11 @@
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=admin
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=2SnixVWTHsU1qvYjLVmE8s3onNeQP8i0ovPVBqkW
+export OS_AUTH_URL=http://1.2.3.4:35357/v3
+export OS_INTERFACE=internal
+export OS_IDENTITY_API_VERSION=3
+export OS_REGION_NAME=RegionOne
+export OS_AUTH_PLUGIN=password
